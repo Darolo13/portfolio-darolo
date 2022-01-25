a7b15012922b6c25385d47664f108620bb1a2ad9
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-25 16:17:22 -0700

    Refactor module file paths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
+import Acomplishments from '../components/Acomplishments';
+import BgAnimation from '../components/BackgrooundAnimation';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+import Technologies from '../components/Technologies';
+import Timeline from '../components/TimeLine';
+import { Layout } from '../layout';
 import { Section } from '../styles/GlobalComponents';
 
 const Home = () => {
