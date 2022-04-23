eebab8526332279b35061be26ce4901f3a18db79
Darolo13 <davidromero1244@gmail.com>
2022-04-22 18:35:51 -0700

    Add metaTitle

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,16 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>David's Portfolio</title>
+        <meta property="og:title" content="David's Portfolio" key="title" />
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
